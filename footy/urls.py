16082be006262d97ff20bf3adcab9ed79867815d
Charlie Shepherd <cs648@cam.ac.uk>
2012-09-10 17:28:19 +0200

    Fix admin portion of site (currently unused)

--- footy/urls.py
+++ footy/urls.py
@@ -38,9 +38,6 @@ urlpatterns = patterns('',
         make_object_list=True,
     ), name='match_year_list'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
